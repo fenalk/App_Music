45d7b8955c59eebf0b9104b8821fa797a2f1b7e1
Fernanda Borges <69552512+fenalk@users.noreply.github.com>
2024-11-03 19:45:05 -0300

    Create InterfacePlayer.java

---
+++ Interface do Player/InterfacePlayer.java
@@ -0,0 +1,11 @@
+public interface InterfacePlayer {
+        //comportamentos que de classes que precisam reproduzir musica irão manifestar
+        public abstract void play();
+        public abstract void pause();
+        public abstract void passar_musica();
+        public abstract void voltar_musica();
+        public abstract void aumentarVolume();
+        public abstract void diminuirVolume();
+        public abstract void favoritar(); 
+    
+}
